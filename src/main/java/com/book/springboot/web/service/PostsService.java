f1f105abfa05aa7def9090082141778dbf729c95
swhyeon98 <smreo75@gmail.com>
2023-07-11 18:00:20 +0900

    feat: 게시글 저장 서비스 추가

---
+++ src/main/java/com/book/springboot/web/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.book.springboot.web.service;
+
+import com.book.springboot.web.domain.posts.PostsRepository;
+import com.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
