a0145a76e1d7df442753febeb1de2073b02192eb
swhyeon98 <smreo75@gmail.com>
2023-07-11 17:59:55 +0900

    feat: 게시글 생성 API 컨트롤러 추가

---
+++ src/main/java/com/book/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.book.springboot.web;
+
+import com.book.springboot.web.dto.PostsSaveRequestDto;
+import com.book.springboot.web.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
