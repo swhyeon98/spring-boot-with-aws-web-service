ded49ccca33351d315569b8cef8d63af596a7fc6
swhyeon98 <smreo75@gmail.com>
2023-07-26 20:39:41 +0900

    feat: 게시글 저장 컨트롤러 메서드 추가

--- src/main/java/com/book/springboot/web/IndexController.java
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -1,16 +1,18 @@
 package com.book.springboot.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("title", "스프링 부트 웹서비스");
-        model.addAttribute("header", "스프링 부트로 시작하는 웹 서비스");
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
