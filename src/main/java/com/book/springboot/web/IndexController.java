193fb08dd24467fca8a4bdf74974584d1add7b9b
swhyeon98 <smreo75@gmail.com>
2023-07-26 19:17:17 +0900

    fix: Mustache에서 Thymeleaf로의 전환을 통한 한글 인코딩 문제 해결
    Mustache 사용 시 발생한 한글 인코딩 문제를 해결하기 위해 Thymeleaf로 템플릿 엔진을 변경, 이에 따라 관련 코드를 수정.

--- src/main/java/com/book/springboot/web/IndexController.java
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -1,13 +1,16 @@
 package com.book.springboot.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("title", "스프링 부트 웹서비스");
+        model.addAttribute("header", "스프링 부트로 시작하는 웹 서비스");
         return "index";
     }
 }
